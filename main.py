9f8f9bb22a0d642407ecbff1afddb49a9ddb063d
Ronaldo Medeiros <ronaldmedeiiros@gmail.com>
2024-12-06 18:31:08 -0400

    ajuste rota2

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fakepinterest import app
 
 if __name__ == "__main__":
     # Escuta em todas as interfaces (0.0.0.0) e na porta 5000
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5010, debug=True)
